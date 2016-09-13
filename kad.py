da1e2e627ef75d1cc5423d7926ff991264c709ca
Larry Reaves <larry@yrral.net>
2016-09-13 00:49:54 -0400

    fullscreen, ctrl-q to quit, default protocol to http

--- kad.py
+++ kad.py
@@ -13,22 +13,26 @@ class KAD:
         self.builder.connect_signals(self)
 
         window = self.builder.get_object("main_window")
+        self.location_entry = self.builder.get_object("location_entry")
+
         accelerators = Gtk.AccelGroup()
         window.add_accel_group(accelerators)
         key, mod = Gtk.accelerator_parse("<Control>l")
-        self.location_entry = self.builder.get_object("location_entry")
         self.location_entry.add_accelerator("grab-focus", accelerators, key, mod, Gtk.AccelFlags.VISIBLE)
-        browser_window = self.builder.get_object("browser_window")
+        key, mod = Gtk.accelerator_parse("<Control>q")
+        Gtk.AccelGroup.connect(accelerators, key, mod, Gtk.AccelFlags.VISIBLE, Gtk.main_quit)
 
+        browser_window = self.builder.get_object("browser_window")
         self.browser_view = WebKit2.WebView()
         self.browser_view.connect("load-changed", self.load_changed)
         browser_window.add(self.browser_view)
 
-        window.maximize()
-        window.show_all()
         self.location_entry.set_text("http://www.google.com")
         self.location_entry_activate()
         self.browser_view.get_settings().set_property("enable-developer-extras",True)
+
+        window.fullscreen()
+        window.show_all()
 
     def load(self, uri):
         self.browser_view.load_uri(uri)
@@ -44,10 +48,12 @@ class KAD:
 
     def location_entry_activate(self, *args):
         uri = self.location_entry.get_text()
+        if not("http" in uri):
+            uri = "http://" + uri
         self.load(uri)
 
     def load_changed(self, *args):
-        if (args[1] == WebKit2.LoadEvent.FINISHED):
+        if args[1] == WebKit2.LoadEvent.FINISHED:
             uri = self.browser_view.get_uri()
             self.location_entry.set_text(uri)
 
