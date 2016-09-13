957385b735bc8e07efd708c7b7095a6d96a5283c
Larry Reaves <larry@yrral.net>
2016-09-13 00:22:17 -0400

    added control-l shortcut, set location bar after navigate

--- kad.py
+++ kad.py
@@ -13,14 +13,21 @@ class KAD:
         self.builder.connect_signals(self)
 
         window = self.builder.get_object("main_window")
+        accelerators = Gtk.AccelGroup()
+        window.add_accel_group(accelerators)
+        key, mod = Gtk.accelerator_parse("<Control>l")
+        self.location_entry = self.builder.get_object("location_entry")
+        self.location_entry.add_accelerator("grab-focus", accelerators, key, mod, Gtk.AccelFlags.VISIBLE)
         browser_window = self.builder.get_object("browser_window")
 
         self.browser_view = WebKit2.WebView()
+        self.browser_view.connect("load-changed", self.load_changed)
         browser_window.add(self.browser_view)
 
         window.maximize()
         window.show_all()
-        self.load("http://www.google.com")
+        self.location_entry.set_text("http://www.google.com")
+        self.location_entry_activate()
         self.browser_view.get_settings().set_property("enable-developer-extras",True)
 
     def load(self, uri):
@@ -36,9 +43,13 @@ class KAD:
         print self.browser_view.get_uri()
 
     def location_entry_activate(self, *args):
-        entry = self.builder.get_object('location_entry')
-        uri = entry.get_buffer().get_text()
+        uri = self.location_entry.get_text()
         self.load(uri)
+
+    def load_changed(self, *args):
+        if (args[1] == WebKit2.LoadEvent.FINISHED):
+            uri = self.browser_view.get_uri()
+            self.location_entry.set_text(uri)
 
 kad = KAD()
 kad.main()
