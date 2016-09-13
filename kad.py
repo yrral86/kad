7e1d42207899ce4d6285a1059fd52947dc29f4d7
Larry Reaves <larry@yrral.net>
2016-09-12 23:47:50 -0400

    more kad

--- kad.py
+++ kad.py
@@ -1,19 +1,44 @@
+#!/usr/bin/env python
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 gi.require_version('WebKit2', '4.0')
 from gi.repository import WebKit2
 
-builder = Gtk.Builder()
-builder.add_from_file("shell.glade")
+class KAD:
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("shell.glade")
+        self.builder.connect_signals(self)
 
-window = builder.get_object("mainWindow")
-browserWindow = builder.get_object("browserWindow")
+        window = self.builder.get_object("main_window")
+        browser_window = self.builder.get_object("browser_window")
 
-browserView = WebKit2.WebView()
-browserWindow.add(browserView)
+        self.browser_view = WebKit2.WebView()
+        browser_window.add(self.browser_view)
 
-window.show_all()
-browserView.open("http://www.google.com")
+        window.maximize()
+        window.show_all()
+        self.load("http://www.google.com")
+        self.browser_view.get_settings().set_property("enable-developer-extras",True)
 
-Gtk.main()
+    def load(self, uri):
+        self.browser_view.load_uri(uri)
+
+    def main(self):
+        Gtk.main()
+
+    def main_window_delete(self, *args):
+        Gtk.main_quit(*args)
+
+    def save_button_clicked(self, *args):
+        print self.browser_view.get_uri()
+
+    def location_entry_activate(self, *args):
+        entry = self.builder.get_object('location_entry')
+        uri = entry.get_buffer().get_text()
+        self.load(uri)
+
+kad = KAD()
+kad.main()
