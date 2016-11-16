1cde64e1d8fbb27313bbebd8d54cf3657c921a0f
Larry Reaves <larry@yrral.net>
2016-11-16 17:33:24 -0500

    relative paths, add geckodriver

--- webcawler.py
+++ webcawler.py
@@ -3,14 +3,14 @@ def webcawler(keyword,pagenum,year):
     import os
     from selenium import webdriver
     from selenium.webdriver.support.ui import WebDriverWait
-    driver = webdriver.Firefox(executable_path = '/home/shifu/Downloads/geckodriver')
+    driver = webdriver.Firefox(executable_path = 'geckodriver/geckodriver')
     driver.get('http://scholar.google.com')
     inputElement = driver.find_element_by_name("q")
     inputElement.send_keys(keyword)
     inputElement.submit()
     currentURL=driver.current_url
     urlList=[]
-    localDir = '/home/shifu/Downloads/kad-larry/pdf/'
+    localDir = 'pdf/'
     fileOut = localDir + keyword + ".txt"
     import urllib, re,codecs,sys
     fileOp = codecs.open(fileOut, 'a', sys.getdefaultencoding())
@@ -29,20 +29,20 @@ def webcawler(keyword,pagenum,year):
         contents=driver.find_elements_by_css_selector('h3')
         for ct in contents:
             print ct.text
-            
+
         driver.get(currentURL+"scholar?hl=en&q="+keyword+"&start="+str(i*10)+"&as_sdt=0,5&as_ylo="+year)
         import time
         time.sleep(3)
     print len(urlList)
 
-    for everyURL in urlList:                                 
-            wordItems = everyURL.split('/')                   
-            for item in wordItems:                            
-                    if re.match('.*\.pdf$', item):            
-                            PDFName = item                  
-            localPDF = localDir +keyword+"_"+ PDFName                   
+    for everyURL in urlList:
+            wordItems = everyURL.split('/')
+            for item in wordItems:
+                    if re.match('.*\.pdf$', item):
+                            PDFName = item
+            localPDF = localDir +keyword+"_"+ PDFName
             try:
-                    urllib.urlretrieve(everyURL, localPDF)   
+                    urllib.urlretrieve(everyURL, localPDF)
             except Exception,e:
                     continue
-    
+
